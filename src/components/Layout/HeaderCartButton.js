32b35d685df4775df96edb2904f7dc98ab808af4
Tasneem Patrawala <tasneemp_sa@yahoo.com>
2023-06-24 07:53:55 -0400

    Add Cart logic

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,24 @@
+import { useContext } from 'react';
+
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+
+  const cartCtx = useContext(CartContext);
+
+  const numberOfItems = cartCtx.items.reduce((accum, item) => {
+    return accum + item.amount;
+  }, 0)
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   );
 };
